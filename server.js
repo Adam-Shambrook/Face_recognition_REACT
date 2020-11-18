416deaf9b86ca7ae4a74f789b7de5eef97505828
Adam-Shambrook <shambrookadam1@gmail.com>
2020-11-18 08:26:43 +0000

    deploying

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/Register');
+const signIn = require('./controllers/signIn');
+const profile = require('./controllers/Profile');
+const image = require('./controllers/Image');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host : '127.0.0.1',
+		user : '',
+		password: '',
+		database : 'smart-brain'
+	}
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {res.send(database.users)})
+app.post('/signin', (req , res ) => { signIn.handleSignIn(req , res , db , bcrypt)})
+app.post('/register', (req , res ) => { register.handleRegister(req, res, db, bcrypt, saltRounds)})
+app.get('/profile/:id', (req , res ) => { profile.handleProfile(req, res, db)})
+app.put('/image', (req , res ) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req , res ) => { image.handleApiCall(req, res, db)})
+
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000')
+})
+
+
